cfef5c53c48f5caacd5b6d68cdbd6e6d938d2520
Ajeet Yadav <ajeetkr35@gmail.com>
2019-01-03 23:12:30 +0530

    first-commit

--- app/src/main/java/com/dao/ajeet/sqlitetest/DbHelper.java
+++ app/src/main/java/com/dao/ajeet/sqlitetest/DbHelper.java
@@ -1,4 +1,72 @@
 package com.dao.ajeet.sqlitetest;
 
-public class DbHelper {
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
+import static android.provider.BaseColumns._ID;
+
+import static android.provider.BaseColumns._ID;
+
+/**
+ * Created by delaroy on 9/8/17.
+ */
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final String TAG = DbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "image.db";
+    private static final int DATABASE_VERSION = 1;
+    Context context;
+    SQLiteDatabase db;
+    ContentResolver mContentResolver;
+
+    public final static String COLUMN_NAME = "imagename";
+    public final static String COLUMN_NAME_1= "name";
+
+    public final static String TABLE_NAME = "imagetable";
+
+
+
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+        mContentResolver = context.getContentResolver();
+
+        db = this.getWritableDatabase();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_IMAGE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME + " BLOB NOT NULL, "+
+                COLUMN_NAME_1+ " STRING  "  + " );";
+
+        db.execSQL(SQL_CREATE_IMAGE_TABLE);
+
+        Log.d(TAG, "Database Created Successfully" );
+
+    }
+
+    public void addToDb(byte[] image,String name){
+        ContentValues cv = new  ContentValues();
+        cv.put(COLUMN_NAME,   image);
+        cv.put(COLUMN_NAME_1,   name);
+        db.insert( TABLE_NAME, null, cv );
+        Log.d(TAG, "Database added Successfully" );
+      //  Toast.makeText(context, "added successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
 }
--- app/src/main/java/com/dao/ajeet/sqlitetest/MainActivity.java
+++ app/src/main/java/com/dao/ajeet/sqlitetest/MainActivity.java
@@ -1,13 +1,109 @@
 package com.dao.ajeet.sqlitetest;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button addInDB, pickGallery, showPic;
+    ImageView im;
+    private int EXTERNAL_STORAGE_PERMISSION_CONSTANT = 101;
+    private int PICK_IMAGE=1;
+    DbHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        dbHelper=new DbHelper(this);
+        im = findViewById(R.id.imageView);
+        addInDB = findViewById(R.id.add_db);
+        pickGallery = findViewById(R.id.gallery);
+        showPic = findViewById(R.id.show);
+        pickGallery.setOnClickListener(this);
+        showPic.setOnClickListener(this);
+        addInDB.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.gallery) {
+
+
+
+
+            OpenGallery();
+        }
+        if(view.getId()==R.id.add_db){
+            im.setDrawingCacheEnabled(true);
+            im.buildDrawingCache();
+            Bitmap bitmap = im.getDrawingCache();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+            dbHelper.addToDb(data,"abc");
+
+
+        }
+        if(view.getId()==R.id.show){
+            Intent i=new Intent(MainActivity.this,ShowActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public void OpenGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
+
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if (requestCode == PICK_IMAGE) {
+           Uri uri=data.getData();
+            try {
+                final InputStream inputStream=getContentResolver().openInputStream(uri);
+                final Bitmap bitmap=BitmapFactory.decodeStream(inputStream);
+                im.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == EXTERNAL_STORAGE_PERMISSION_CONSTANT) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+              OpenGallery();
+            } else {
+                Toast.makeText(this, "Permission Required", Toast.LENGTH_SHORT).show();
+
+            }
+        }
     }
 }
+
+
+
